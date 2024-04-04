46a19d44988731689c045df19d65afb6faa3a138
muhammadali0666 <muhammadalishuhratjonov50@gmail.com>
2024-04-05 00:58:27 +0500

    list basic created

--- src/components/ProductList/index.tsx
+++ src/components/ProductList/index.tsx
@@ -1,7 +1,77 @@
-import "./product.css"
+import "./product.css";
+import { RiDeleteBin5Fill } from "react-icons/ri";
 
 export const ProductList = () => {
   return (
-    <div>ProductList</div>
-  )
-}
+    <div className="list">
+      <div className="list-box">
+        <select className="list-select">
+          <option value="men" className="list-option">
+            Men
+          </option>
+          <option value="women" className="list-option">
+            Women
+          </option>
+          <option value="kid" className="list-option">
+            Kids
+          </option>
+        </select>
+        <input type="text" className="list-search" placeholder="search..." />
+      </div>
+      <table>
+        <thead>
+          <tr>
+            <th>No</th>
+            <th>Title</th>
+            <th>Category</th>
+            <th>Old price</th>
+            <th>New price</th>
+            <th>Image</th>
+            <th>Delete</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>0</td>
+            <td>Kiyim</td>
+            <td>men</td>
+            <td>Narx</td>
+            <td>Narx</td>
+            <td>
+              <img src="" alt="cloth img" className="list-image" />
+            </td>
+            <td>
+              <RiDeleteBin5Fill className="list-icon" />
+            </td>
+          </tr>
+          <tr>
+            <td>0</td>
+            <td>Kiyim</td>
+            <td>men</td>
+            <td>Narx</td>
+            <td>Narx</td>
+            <td>
+              <img src="" alt="cloth img" className="list-image" />
+            </td>
+            <td>
+              <RiDeleteBin5Fill className="list-icon" />
+            </td>
+          </tr>
+          <tr>
+            <td>0</td>
+            <td>Kiyim</td>
+            <td>men</td>
+            <td>Narx</td>
+            <td>Narx</td>
+            <td>
+              <img src="" alt="cloth img" className="list-image" />
+            </td>
+            <td>
+              <RiDeleteBin5Fill className="list-icon" />
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+};
